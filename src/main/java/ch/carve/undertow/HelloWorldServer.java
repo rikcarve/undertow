87fba30f5d962bb13d0522baf63df69b4bd8a1ad
rikcarve <rik@carve.ch>
2017-04-25 17:10:26 +0200

    show a way to get connection statistics

--- src/main/java/ch/carve/undertow/HelloWorldServer.java
+++ src/main/java/ch/carve/undertow/HelloWorldServer.java
@@ -4,6 +4,7 @@ import org.slf4j.LoggerFactory;
 
 import io.undertow.Handlers;
 import io.undertow.Undertow;
+import io.undertow.Undertow.ListenerInfo;
 import io.undertow.UndertowOptions;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.handlers.BlockingHandler;
@@ -11,16 +12,20 @@ import io.undertow.server.handlers.RequestLimitingHandler;
 import io.undertow.server.handlers.accesslog.AccessLogHandler;
 
 public class HelloWorldServer {
+    private static Undertow server = null;
 
     public static void main(final String[] args) {
-        Undertow server = Undertow.builder()
+        DelayedHelloWorldHandler myHandler = new DelayedHelloWorldHandler();
+        myHandler.setListenerInfoGetter(HelloWorldServer::getListenerInfo);
+        server = Undertow.builder()
                 .setWorkerThreads(25)
                 .addHttpListener(8080, "localhost")
                 .setServerOption(UndertowOptions.RECORD_REQUEST_START_TIME, true)
+                .setServerOption(UndertowOptions.ENABLE_STATISTICS, true)
                 .setHandler(Handlers.routing()
                         .get("/test", HelloWorldHandler::handleRequest)
                         .get("/test2", wrapWithCommonHandlers(HelloWorldHandler::handleRequest))
-                        .get("/delayed", wrapWithCommonHandlers(DelayedHelloWorldHandler::handleRequest)))
+                        .get("/delayed", wrapWithCommonHandlers(myHandler)))
                 .build();
         server.start();
     }
@@ -33,10 +38,15 @@ public class HelloWorldServer {
     private static HttpHandler limit(HttpHandler next) {
         return new RequestLimitingHandler(5, 1, next);
     }
+
     private static HttpHandler wrapWithCommonHandlers(HttpHandler handler) {
         return HandlerChainBuilder.begin(BlockingHandler::new)
                 .next(HelloWorldServer::accessLog)
                 .next(HelloWorldServer::limit)
                 .complete(handler);
+    }
+
+    private static ListenerInfo getListenerInfo(int index) {
+        return server.getListenerInfo().get(index);
     }
 }
